3b4801821a20733da629b7760c665b6692a08eda
Filipp Gudis <fgudis@yahoo.com>
2020-10-21 19:32:39 -0700

    basic redux state manipulations!

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+  
+
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAdd5Counter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtract5Counter}  />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAdd5Counter: () => dispatch({type: 'ADD5'}),
+        onSubtract5Counter: () => dispatch({type: 'SUBTRACT5'})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
